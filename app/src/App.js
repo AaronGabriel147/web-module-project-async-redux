60be2c40b8060e5a75b52e9756386f8dea989489
Aaron <aarongabriel147@gmail.com>
2021-07-27 09:23:20 -0700

    setUp

---
+++ app/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+// import { applyMiddleware, createStore } from 'redux';
+// import logger from 'redux-logger';
+
+
+// const store = createStore(
+//   reducer,
+//   applyMiddleware(logger)
+// );
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>hi</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
